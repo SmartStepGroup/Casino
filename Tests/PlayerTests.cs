1573d4f5cfa47ab1927b64a4833c5198c6a3b3da
osokolov <osokolov@DEV6-PC>
2016-10-24 15:55:37 +0400

    SetUp

--- Tests/PlayerTests.cs
+++ Tests/PlayerTests.cs
@@ -4,95 +4,48 @@ using NUnit.Framework;
 
 namespace Tests
 {
-    [TestFixture]
+   [TestFixture]
     public class PlayerTests
     {
+      private Player _player;
+
+      [SetUp]
+      public void SetUp()
+      {
+         _player = new Player();
+      }
+
       [Test]
       public void ByDefault_PlayerHasNoChips()
       {
-         var player = new Player();
-
-         Assert.AreEqual(0, player.Chips);
+         Assert.AreEqual(0, _player.Chips);
       }
 
       [Test]
       public void ByDefault_PlayerHasNoBet()
       {
-         var player = new Player();
-
-         Assert.AreEqual(null, player.CurrentBet);
+         Assert.IsNull(_player.CurrentBet);
       }
 
        [Test]
        public void Buy1Chip_NewPlayer_PlayerHas1Chip()
        {
-         var player = new Player();
+         _player.BuyChips(1);
 
-         player.BuyChips(1);
-
-         Assert.AreEqual(1, player.Chips);
+         Assert.AreEqual(1, _player.Chips);
        }
-
-       [Test]
-       public void ChipsIncreaseByInputValue_PlayerHas1Chips_PlayerHas3Chip()
-       {
-         var player = new Player();
-         player.BuyChips(1);
-
-         player.BuyChips(2);
-
-         Assert.AreEqual(3, player.Chips);
-       }
-
+     
        [Test]
        public void BuyNegativeChipsCount_NewPlayer_ArgumentException()
        {
-         var player = new Player();
-         
-         Assert.Catch<ArgumentException>(() => player.BuyChips(-1));
-       }
-      
-      [Test]
-       public void Bet1Chip_PlayerHas1Chip_PlayerHasBet()
-       {
-         var player = new Player();
-         player.BuyChips(1);
-
-         player.Bet(1, 3);
-         
-         Assert.AreNotEqual(null, player.CurrentBet);
-       }
-
-       [Test]
-       public void Bet1Chip_PlayerHas1Chips_PlayerHasNoChips()
-       {
-         var player = new Player();
-         player.BuyChips(1);
-
-         player.Bet(1, 3);
-         
-         Assert.AreEqual(0, player.Chips);
+         Assert.Catch<ArgumentException>(() => _player.BuyChips(-1));
        }
 
        [Test]
        public void Bet1Chip_PlayerHasNoChips_ArgumentException()
        {
-         var player = new Player();
-
-         Assert.Catch<ArgumentException>(() => player.Bet(1, 3));
-         Assert.AreEqual(null, player.CurrentBet);
-       }
-
-       [Test]
-       public void Bet1Chip_PlayerHasNoChipsButHasBet_PlayerNotLostCurrentBet()
-       {
-         var player = new Player();
-         player.BuyChips(1);
-         player.Bet(1, 1);
-         var currentBet = player.CurrentBet;
-
-         Assert.Catch<ArgumentException>(() => player.Bet(1, 3));
-         Assert.AreEqual(currentBet, player.CurrentBet);
+         Assert.Catch<ArgumentException>(() => _player.Bet(1, 3));
+         Assert.AreEqual(null, _player.CurrentBet);
        }
    }
 }
---
+++ Tests/PlayerWith1ChipTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+   [TestFixture]
+   public class PlayerWith1ChipTests
+   {
+      private Player _playerWith1Chip;
+
+      [SetUp]
+      public void SetUp()
+      {
+        var player = new Player();
+         player.BuyChips(1);
+         _playerWith1Chip = player;
+      }
+
+      [Test]
+      public void ChipsIncreaseBy2_PlayerHas3Chip()
+      {
+         _playerWith1Chip.BuyChips(2);
+
+         Assert.AreEqual(1 + 2, _playerWith1Chip.Chips);
+      }
+
+      [Test]
+      public void Bet1Chip_PlayerHasBet()
+      {
+         _playerWith1Chip.Bet(1, 3);
+         
+         Assert.IsNotNull(_playerWith1Chip.CurrentBet);
+         Assert.AreEqual(1, _playerWith1Chip.CurrentBet.Chips);
+      }
+
+      [Test]
+      public void Bet1Chip_PlayerHasNoChips()
+      {
+         _playerWith1Chip.Bet(1, 3);
+         
+         Assert.AreEqual(0, _playerWith1Chip.Chips);
+      }
+
+      
+      [Test]
+      public void Bet1Chip_PlayerHasNoChipsButHasBet_PlayerKeepCurrentBet()
+      {
+         _playerWith1Chip.Bet(1, 1);
+         var currentBet = _playerWith1Chip.CurrentBet;
+
+         try
+         {
+            _playerWith1Chip.Bet(1, 3);
+         }
+         catch { }
+
+         Assert.AreEqual(currentBet, _playerWith1Chip.CurrentBet);
+      }
+   }
+}
