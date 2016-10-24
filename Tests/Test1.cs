5c445fbac140c59a45d12fd41293e958ea2069d6
dbutov <dbutov@elvees.com>
2016-10-24 15:43:00 +0400

    Common Initialize code

--- Tests/Test1.cs
+++ Tests/Test1.cs
@@ -7,24 +7,28 @@ namespace Tests
     [TestFixture]
     public class PlayerTest
     {
-        [Test]
+		private Player player;
+
+		[SetUp]
+		public void Initialize()
+		{
+			player = new Player();
+		}
+
+		[Test]
         public void Player_ByDefault_HasNoChips()
         {
-            var player = new Player();
             Assert.AreEqual(0, player.Chips);
         }
 		[Test]
 		public void Player_Buys1Chips_Sets1Chip()
 		{
-			var player = new Player();
 			player.BuyChips(1);
-
 			Assert.AreEqual(1, player.Chips);
 		}
 		[Test]
 		public void Player_WhenHas1Chip_Buys1ChipsMore_Sets2Chip()
 		{
-			var player = new Player();
 			player.BuyChips(1);
 
 			player.BuyChips(1);
@@ -33,13 +37,11 @@ namespace Tests
 		[Test]
 		public void Player_BuysNegativeChips_ThrowsArgumentException()
 		{
-			var player = new Player();
 			var exception = Assert.Throws<ArgumentException>(() => { player.BuyChips(-1); });
 		}
 		[Test]
 		public void Player_BuysTooManyChips_IntegerOverflow()
 		{
-			var player = new Player();
 			player.BuyChips(int.MaxValue);
 
 			player.BuyChips(1);
@@ -48,24 +50,20 @@ namespace Tests
 		[Test]
 		public void Player_With1ChipBets2Chips_ThrowsArgumentException()
 		{
-			var player = new Player();
 			player.BuyChips(1);
-
 			var exception = Assert.Throws<ArgumentException>(() => { player.Bet(2, 4); });
 		}
 		[Test]
-		public void Player_With1ChipBets1Chip_Sets0Chips()
+		public void Player_With1ChipBets1Chip_Has0Chips()
 		{
-			var player = new Player();
 			player.BuyChips(1);
 
 			player.Bet(1, 4);
 			Assert.AreEqual(0, player.Chips);
 		}
 		[Test]
-		public void Player_With1ChipBets1ChipAndWins6Chips_Sets6Chips()
+		public void Player_With1ChipBets1ChipAndWins6Chips_Has6Chips()
 		{
-			var player = new Player();
 			player.BuyChips(1);
 			player.Bet(1, 4);
 
@@ -75,7 +73,6 @@ namespace Tests
 		[Test]
 		public void Player_Bets_BetIsNotNull()
 		{
-			var player = new Player();
 			player.BuyChips(1);
 			player.Bet(1, 4);
 
