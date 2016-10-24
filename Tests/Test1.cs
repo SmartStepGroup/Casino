8c20fce11b7a07fb7bca4138d5b56559cd9af22a
Admin <Admin@DEV22-PC>
2016-10-24 14:25:41 +0400

    1

--- Tests/Test1.cs
+++ Tests/Test1.cs
@@ -1,17 +1,37 @@
-﻿using Domain;
+﻿using System;
+using Domain;
 using NUnit.Framework;
 
 namespace Tests
 {
     [TestFixture]
-    public class Test1
+    public class PlayerTest
     {
         [Test]
-        public void TrueIsTrue()
+        public void Default_CreatePlayer_0Chips()
+        {
+            var player = new Player();
+
+            Assert.AreEqual(0, player.Chips);
+        }
+
+        [Test]
+        public void BuyChips_1_Have1()
         {
             var player = new Player();
             player.BuyChips(1);
-            Assert.True(true);
+
+            Assert.AreEqual(1, player.Chips);
+        }
+
+        [Test]
+        public void BuyChips_minus1_ExceptionArgument()
+        {
+            var player = new Player();
+
+
+            var exception = Assert.Catch<ArgumentException>(() => { player.BuyChips(-1); });
+            Assert.AreEqual("Are you cheating?", exception.Message);
         }
     }
 }
