1091c93cdaf1a1d1dd290126c80c8cecc36c2635
osokolov <osokolov@DEV6-PC>
2016-10-24 13:33:05 +0400

    Тест для Ставки и Фишек

---
+++ Tests/PlayerTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+      [Test]
+      public void ByDefault_PlayerHasNoChips()
+      {
+         var player = new Player();
+
+         Assert.AreEqual(0, player.Chips);
+      }
+
+      [Test]
+      public void ByDefault_PlayerHasNoBet()
+      {
+         var player = new Player();
+
+         Assert.AreEqual(null, player.CurrentBet);
+      }
+
+       [Test]
+       public void Buy1Chip_NewPlayer_PlayerHas1Chip()
+       {
+         var player = new Player();
+
+         player.BuyChips(1);
+
+         Assert.AreEqual(1, player.Chips);
+       }
+
+       [Test]
+       public void ChipsIncreaseByInputValue_PlayerHas1Chips_PlayerHas3Chip()
+       {
+         var player = new Player();
+         player.BuyChips(1);
+
+         player.BuyChips(2);
+
+         Assert.AreEqual(3, player.Chips);
+       }
+
+       [Test]
+       public void BuyNegativeChipsCount_NewPlayer_ArgumentException()
+       {
+         var player = new Player();
+         
+         Assert.Catch<ArgumentException>(() => player.BuyChips(-1));
+       }
+      
+      [Test]
+       public void Bet1Chip_PlayerHas1Chip_PlayerHasBet()
+       {
+         var player = new Player();
+         player.BuyChips(1);
+
+         player.Bet(1, 3);
+         
+         Assert.AreNotEqual(null, player.CurrentBet);
+       }
+
+      [Test]
+       public void Bet1Chip_PlayerHas1Chips_PlayerHasNoChips()
+       {
+         var player = new Player();
+         player.BuyChips(1);
+
+         player.Bet(1, 3);
+         
+         Assert.AreEqual(0, player.Chips);
+       }
+   }
+}
