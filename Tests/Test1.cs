9f476a83adf0023ea9d6bf3f71c865ac2ec046b7
Admin <Admin@DEV22-PC>
2016-10-24 16:55:49 +0400

    2

--- Tests/Test1.cs
+++ Tests/Test1.cs
@@ -7,31 +7,143 @@ namespace Tests
     [TestFixture]
     public class PlayerTest
     {
-        [Test]
-        public void Default_CreatePlayer_0Chips()
-        {
-            var player = new Player();
+        private Player _player;
 
-            Assert.AreEqual(0, player.Chips);
+        [SetUp]
+        public void RegisterNewPlayer()
+        {
+            _player = new Player();
         }
 
         [Test]
-        public void BuyChips_1_Have1()
+        public void Default_CreatePlayer_0Chips()
         {
-            var player = new Player();
-            player.BuyChips(1);
+            Assert.AreEqual(0, _player.Chips);
+        }
 
-            Assert.AreEqual(1, player.Chips);
+        [Test]
+        public void BuyChips_0_Has0()
+        {
+            _player.BuyChips(0);
+
+            Assert.AreEqual(0, _player.Chips);
+        }
+
+        [Test]
+        public void BuyChips_1_Has1()
+        {
+            _player.BuyChips(1);
+
+            Assert.AreEqual(1, _player.Chips);
         }
 
         [Test]
         public void BuyChips_minus1_ExceptionArgument()
         {
-            var player = new Player();
-
-
-            var exception = Assert.Catch<ArgumentException>(() => { player.BuyChips(-1); });
+            var exception = Assert.Catch<ArgumentException>(() => { _player.BuyChips(-1); });
             Assert.AreEqual("Are you cheating?", exception.Message);
         }
+
+        [Test]
+        public void Lose_Void_Null()
+        {
+            _player.Bet(0, 1);
+            _player.Lose();
+
+            Assert.IsNull(_player.CurrentBet);
+        }
+
+        [Test]
+        public void Win_1Chip_1More()
+        {
+            var chipsBeforeWin = _player.Chips;
+            _player.Win(1);
+
+            Assert.AreEqual(chipsBeforeWin + 1, _player.Chips);
+        }
+
+        [Test]
+        public void Win_1Chip_CurrentBetIsNull()
+        {
+            _player.Win(1);
+
+            Assert.IsNull(_player.CurrentBet);
+        }
+
+
+        [Test]
+        public void Bet_Has1_BetChips2()
+        {
+            _player.BuyChips(1);
+
+            Assert.Catch<ArgumentException>(() => { _player.Bet(chips: 2, score: 1); });
+        }
+
+        [Test]
+        public void Bet_BetChips1_HasMinus1()
+        {
+            _player.BuyChips(1);
+            var chipsBeforeBet = _player.Chips;
+            _player.Bet(1, 1);
+
+            Assert.AreEqual(chipsBeforeBet - 1, _player.Chips);
+        }
+
+        [Test]
+        public void Bet_1Chip_CurrentBetIsNotNull()
+        {
+            _player.BuyChips(1);
+
+            _player.Bet(1, 1);
+
+            Assert.IsNotNull(_player.CurrentBet);
+        }
+
+        [Test]
+        public void Bet_BetChipsM1_HasMinus1()
+        {
+            _player.BuyChips(1);
+
+            Assert.Catch<ArgumentException>(() => { _player.Bet(-1, 1); });
+        }
+
+        [Test]
+        public void Join()
+        {
+            var game = new RollDiceGame();
+
+            _player.Join(game);
+
+            Assert.IsNotNull(game.Player);
+        }
+
+        [Test]
+        public void Leave()
+        {
+            var game = new RollDiceGame();
+
+            _player.Leave(game);
+
+            Assert.IsNull(game.Player);
+        }
     }
+
+    [TestFixture]
+    public class RollDiceGameTest
+    {
+
+        [Test]
+        public void Run()
+        {
+            var _rollDiceGame = new RollDiceGame();
+            _rollDiceGame.Player = new Player();
+            _rollDiceGame.Player.BuyChips(1);
+            _rollDiceGame.Player.Bet(1,2);
+
+            _rollDiceGame.Play();
+
+            Assert.AreEqual(0, _rollDiceGame.Player.Chips);
+        }
+    }
+
 }
