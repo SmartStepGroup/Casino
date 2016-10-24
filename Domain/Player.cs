de52f3134042eb607e68049aded40755ff3d3ab1
vdvoranchicov <vdvoranchicov@VEDUN-PC>
2016-10-24 13:32:12 +0400

    Правка логики переполнения.

--- Domain/Player.cs
+++ Domain/Player.cs
@@ -1,42 +1,55 @@
 ﻿using System;
 
-namespace Domain {
-    public class Player {
-        public Bet CurrentBet { get; private set; }
+namespace Domain
+{
+   public class Player
+   {
+      public Bet CurrentBet { get; private set; }
 
-        public void BuyChips(int chips) {
-            if (chips < 0) {
-                throw new ArgumentException("Are you cheating?");
-            }
+      public void BuyChips(int chips)
+      {
+         if (chips < 0)
+         {
+            throw new ArgumentException("Are you cheating?");
+         }
+         checked
+         {
             Chips += chips;
-        }
+         }
+      }
 
-        public int Chips { get; private set; }
+      public int Chips { get; private set; }
 
-        public void Bet(int chips, int score) {
-            if (Chips < chips) {
-                throw new ArgumentException("Have you spend all your money already?");
-            }
+      public void Bet(int chips, int score)
+      {
+         if (Chips < chips)
+         {
+            throw new ArgumentException("Have you spend all your money already?");
+         }
 
-            Chips -= chips;
-            CurrentBet = new Bet(chips, score);
-        }
+         Chips -= chips;
+         CurrentBet = new Bet(chips, score);
+      }
 
-        public void Lose() {
-            CurrentBet = null;
-        }
+      public void Lose()
+      {
+         CurrentBet = null;
+      }
 
-        public void Win(int chips) {
-            Chips += chips;
-            CurrentBet = null;
-        }
+      public void Win(int chips)
+      {
+         Chips += chips;
+         CurrentBet = null;
+      }
 
-        public void Join(RollDiceGame game) {
-            game.Player = this;
-        }
+      public void Join(RollDiceGame game)
+      {
+         game.Player = this;
+      }
 
-        public void Leave(RollDiceGame game) {
-            game.Player = null;
-        }
-    }
+      public void Leave(RollDiceGame game)
+      {
+         game.Player = null;
+      }
+   }
 }
