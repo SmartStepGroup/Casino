1f167f7f72e0509bc8016b56da60ceeb226c89b0
vdvoranchicov <vdvoranchicov@VEDUN-PC>
2016-10-24 13:27:37 +0400

    Добавление новых тестов.

--- Tests/Test1.cs
+++ Tests/Test1.cs
@@ -1,17 +1,67 @@
-﻿using Domain;
+﻿using System;
+using Domain;
 using NUnit.Framework;
 
 namespace Tests
 {
-    [TestFixture]
-    public class Test1
-    {
-        [Test]
-        public void TrueIsTrue()
-        {
-            var player = new Player();
-            player.BuyChips(1);
-            Assert.True(true);
-        }
-    }
+   [TestFixture]
+   public class PlayerTest
+   {
+      [Test]
+      public void ByDefault_ChipsIs0()
+      {
+         var player = new Player();
+
+         Assert.AreEqual(0, player.Chips);
+      }
+
+      [Test]
+      public void Buy1Chip_ChipsIs1()
+      {
+         var player = new Player();
+
+         player.BuyChips(1);
+
+         Assert.AreEqual(1, player.Chips);
+      }
+
+      [Test]
+      public void BuyNegativeChip1_ThrowArgumentException()
+      {
+         var player = new Player();
+
+         var exception = Assert.Catch<ArgumentException>(() => player.BuyChips(-1));
+
+         Assert.AreEqual("Are you cheating?", exception.Message);
+      }
+
+      [Test]
+      public void BuyChip0_ChipsIs0()
+      {
+         var player = new Player();
+
+         player.BuyChips(0);
+
+         Assert.AreEqual(0, player.Chips);
+      }
+
+      [Test]
+      public void BuyMaxIntChips_ChipsIsIntMax()
+      {
+         var player = new Player();
+
+         player.BuyChips(int.MaxValue);
+
+         Assert.AreEqual(int.MaxValue, player.Chips);
+      }
+
+      [Test]
+      public void BuyMaxIntChips_HaveChips1_ThrowOverflowException()
+      {
+         var player = new Player();
+         player.BuyChips(1);
+
+         var exception = Assert.Catch<OverflowException>(() => player.BuyChips(int.MaxValue));
+      }
+   }
 }
