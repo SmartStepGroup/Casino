d7d79d23e534788df3ed31044aebc559ff367bea
Admin <Admin@DEV22-PC>
2016-10-25 16:53:08 +0400

    new

--- Tests/RollDiceGameTest.cs
+++ Tests/RollDiceGameTest.cs
@@ -10,59 +10,29 @@ namespace Tests
         [Test]
         public void Play_Has1ChipAndBetForLuckyScrore_Win()
         {
-            var player = CreatePlayer.BuyChips(1).BetChips(1).ForScore(6);
-            CreateGame.Join(player).WaitWinResult(6).CheckResult();
-            
+            var player = NewPlayer.BuyChips(1).BetChips(1).ForScore(6);
+            NewGame.WithWinScore(6).Join(player).Play();
+
             Assert.AreEqual(1*6, player.Chips);
         }
 
         [Test]
         public void Play_Has1ChipAndBetForLuckyScrore_Lose()
         {
-            var player = CreatePlayer.BuyChips(1).BetChips(1).ForScore(6);
-            CreateGame.Join(player).WaitWinResult(1).CheckResult();
+            var player = NewPlayer.BuyChips(1).BetChips(1).ForScore(6);
+            NewGame.WithWinScore(1).Join(player).Play();
 
             Assert.AreEqual(0, player.Chips);
         }
     }
 
-    public static class CreateGame
-    {
-        static readonly RollDiceGame _game;
-        static Mock<IDice> _dice;
-
-        static CreateGame()
-        {
-            _game = new RollDiceGame();
-        }
-
-        public static RollDiceGame Join(Player player)
-        {
-            _game.Player = player;
-            return _game;
-        }
-
-        public static RollDiceGame WaitWinResult(this RollDiceGame game, int v)
-        {
-            _dice = new Mock<IDice>();
-            _dice.Setup(_ => _.Roll()).Returns(v);
-            
-            return _game;
-        }
-
-        public static void CheckResult(this RollDiceGame game)
-        {
-            _game.Play(_dice.Object);
-        }
-    }
-
-    public static class CreatePlayer
+    public static class NewPlayer
     {
         static readonly Player _player;
         static int _chips;
         static int _score;
 
-        static CreatePlayer()
+        static NewPlayer()
         {
             _player = new Player();
         }
@@ -84,6 +54,36 @@ namespace Tests
             _score = v;
             _player.Bet(_chips, _score);
             return _player;
+        }
+    }
+
+    public static class NewGame
+    {
+        static readonly RollDiceGame _game;
+        static Mock<IDice> _dice;
+
+        static NewGame()
+        {
+            _game = new RollDiceGame();
+        }
+
+        public static RollDiceGame Join(this RollDiceGame game, Player player)
+        {
+            _game.Player = player;
+            return _game;
+        }
+
+        public static RollDiceGame WithWinScore(int v)
+        {
+            _dice = new Mock<IDice>();
+            _dice.Setup(_ => _.Roll()).Returns(v);
+
+            return _game;
+        }
+
+        public static void Play(this RollDiceGame game)
+        {
+            _game.Play(_dice.Object);
         }
     }
 }
