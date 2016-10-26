459a4781bf9f5041475218d6fbffc7315fe86cba
dbutov <dbutov@elvees.com>
2016-10-26 12:25:35 +0400

    First test Join_IsInGame

--- Tests/PlayerTests.cs
+++ Tests/PlayerTests.cs
@@ -1,51 +1,35 @@
-﻿using System;
-using Domain;
+﻿using System.Security.Permissions;
+using System.Security.Policy;
 using NUnit.Framework;
 
 namespace Tests
 {
-   [TestFixture]
+    [TestFixture]
     public class PlayerTests
     {
-      private Player _player;
+        [Test]
+        public void Join_IsInGame()
+        {
+            var player = new Player();
+            var game = new Game();
 
-      [SetUp]
-      public void SetUp()
-      {
-         _player = new Player();
-      }
+            player.Join(game);
+                
+            Assert.IsTrue(player.InGame);
+        }
+    }
 
-      [Test]
-      public void ByDefault_PlayerHasNoChips()
-      {
-         Assert.AreEqual(0, _player.Chips);
-      }
+    public class Game
+    {
+    }
 
-      [Test]
-      public void ByDefault_PlayerHasNoBet()
-      {
-         Assert.IsNull(_player.CurrentBet);
-      }
+    public class Player
+    {
+        public void Join(Game game)
+        {
+           
+        }
 
-       [Test]
-       public void Buy1Chip_NewPlayer_PlayerHas1Chip()
-       {
-         _player.BuyChips(1);
-
-         Assert.AreEqual(1, _player.Chips);
-       }
-     
-       [Test]
-       public void BuyNegativeChipsCount_NewPlayer_ArgumentException()
-       {
-         Assert.Catch<ArgumentException>(() => _player.BuyChips(-1));
-       }
-
-       [Test]
-       public void Bet1Chip_PlayerHasNoChips_ArgumentException()
-       {
-         Assert.Catch<ArgumentException>(() => _player.Bet(1, 3));
-         Assert.AreEqual(null, _player.CurrentBet);
-       }
-   }
+        public bool InGame = true;
+    }
 }
