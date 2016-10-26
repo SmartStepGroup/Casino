b5f931d269c8c9f85030a286ba12a5a17c8226dd
Admin <Admin@DEV22-PC>
2016-10-26 12:22:57 +0400

    TDD 1

--- Domain/RollDiceGame.cs
+++ Domain/RollDiceGame.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-
-namespace Domain {
-    public class RollDiceGame {
-        
-        public Player Player { get; set; }
-
-        public void Play(IDice dice) {
-            var winningScore = dice.Roll();
-            if (Player.CurrentBet.Score == winningScore) {
-                Player.Win(Player.CurrentBet.Chips * 6);
-            }
-            else {
-                Player.Lose();
-            }
-        }
-    }
-
-    public interface IDice
+﻿namespace Domain
+{
+    public class RollDiceGame
     {
-       int Roll();
+        public void Add(Player player)
+        {
+        }
     }
 }
---
+++ Tests/PlayerCan.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PlayerCan
+    {
+        [Test]
+        public void JoinGame()
+        {
+            var player = new Player();
+            var game = new RollDiceGame();
+
+            player.Join(game);
+
+            Assert.True(player.IsInGame);
+        }
+
+        [Test]
+        public void ByDefault_NotInGame()
+        {
+            var player = new Player();
+
+            Assert.False(player.IsInGame);
+        }
+    }
+}
