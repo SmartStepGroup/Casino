c0c37f613ddf1bc8610556d7351cd03b501c2d7f
Evgeny Levin <evgeny@atomnas.ru>
2016-10-26 11:35:14 +0300

    Первый тест "Игрок может войти в игру".

---
+++ Domain.TDD/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.TDD
+{
+    public class Player
+    {
+        public void Join(Game game)
+        {
+            IsInGame = true;
+        }
+
+        public bool IsInGame { get; private set; }
+    }
+}
---
+++ Tests.TDD/PlayerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Domain;
+using Domain.TDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Tests.TDD
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void Join_Player_InGame()
+        {
+            Player player = new Player();
+
+            Game game = new Game();
+
+            player.Join(game);
+
+            Assert.IsTrue(player.IsInGame);
+        }
+    }
+}
